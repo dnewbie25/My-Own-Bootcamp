742633d83c6cf964030d5fcf30625d8b554619ca
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2025-08-08 13:28:04 -0500

    refactored the main.jsx to remove unused functions and imports

--- month-2/notes-app/src/main.jsx
+++ month-2/notes-app/src/main.jsx
@@ -1,10 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./routes/Home.jsx";
-import Notes from "./routes/Notes.jsx";
-import User from "./routes/User.jsx";
-import NotFoundPage from "./routes/NotFoundPage.jsx";
 import "./index.css";
 import App from "./App.jsx";
 
